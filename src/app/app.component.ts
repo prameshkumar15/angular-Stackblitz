c58141a8e3dbcc23c8307f295178261672553ba3
prameshkumar15 <68468113+prameshkumar15@users.noreply.github.com>
2022-10-05 11:21:33 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name1 = 'Angular ' + VERSION.major;
+  onSubmitUser = (user) => {
+    console.log(user);
+  };
 }
